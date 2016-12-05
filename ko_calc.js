7eed8ee02d5f1a1cec8b98aaac6bf931ba45d042
Drew Ferris <ddf8029@gmail.com>
2016-12-05 14:06:04 -0800

    made ajax call without jquery, wrote new calc_lib.js for only one input

--- ko_calc.js
+++ ko_calc.js
@@ -4,6 +4,10 @@
     self.in1 = ko.observable('');
     self.in2 = ko.observable('');
     self.result = ko.observable('');
+    self.resData = ko.observable('');
+    self.plainJSON = ko.toJSON(self.resData);
+
+
     self.add = function() {
       self.result(Number(self.in1()) + Number(self.in2()));
     };
@@ -20,6 +24,26 @@
       self.result('');
       self.in1('');
       self.in2('');
+    };
+
+    self.loadXMLDoc = function() {
+      var xmlhttp = new XMLHttpRequest();
+
+      xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+          if (xmlhttp.status == 200) {
+            document.getElementById('myDiv').innerHTML = xmlhttp.responseText;
+            self.resData(JSON.parse(xmlhttp.response));
+          } else if (xmlhttp.status == 400) {
+            alert('There was an error 400');
+          } else {
+            alert('something else other than 200 was returned');
+          }
+        }
+      };
+
+      xmlhttp.open('GET', 'https://api.github.com/users/drewferris', true);
+      xmlhttp.send();
     };
   };
 
